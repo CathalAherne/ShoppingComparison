378c81126be7886332ac9086f98aec10784b926d
Cathal Aherne <Cathal@hanas-mbp.home>
2019-06-13 22:28:29 +0100

    removed merge conflicts

--- src/main/java/ShoppingItem.java
+++ src/main/java/ShoppingItem.java
@@ -5,16 +5,16 @@ public class ShoppingItem {
 
     private Map itemDescription;
 
-    public ShoppingItem() {
+    public ShoppingItem(String brand, String product, String name, int size) {
         itemDescription = new HashMap();
-        populateItemName();
+        populateItemName(brand, product, name, size);
     }
 
-    private void populateItemName(){
-        itemDescription.put("Brand", null);
-        itemDescription.put("Product", null);
-        itemDescription.put("Name", null);
-        itemDescription.put("Size", null);
+    private void populateItemName(String brand, String product, String name, int size){
+        itemDescription.put("brand", ((brand == null) ? null: brand));
+        itemDescription.put("product", ((brand == null) ? null: product));
+        itemDescription.put("name", ((brand == null) ? null: name));
+        itemDescription.put("size", ((brand == null) ? null: Integer.toString(size)));
     }
 
     public Map getProperties(){
